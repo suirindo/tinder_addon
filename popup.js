d8ae6f4b2c61f945bc8225debb88ed7b0ffbdcb2
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-30 16:31:21 +0900

    fin

--- popup.js
+++ popup.js
@@ -1,34 +1,19 @@
-// const pushMessageToContent = function (message) {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//         const tab = tabs[0]
-//         chrome.tab.sendMessage(tab.id, message)
-//     })
-// }
-
-// document.getElementById('button').addEventListener('click', function() {
-//     const count = getCount()
-//     pushMessageToContent({type: 'like', count: count})
-// })
-
-// const getCount = function() {
-//     const input = document.getElementById('count')
-//     return parseInt(input.value)
-// }
-
 const pushMessageToContent = function (message) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       const tab = tabs[0]
-      chrome.tabs.sendMessage(tab_id, message)
+      chrome.tabs.sendMessage(tabs[0].id, message)
     })
    }
-   
-   document.getElementById('button').addEventListener('click', function() {
-    const count = getCount()
-    pushMessageToContent({type: 'like', count: count}) // ここを変更
-   })
-   
-   const getCount = function () {
+
+    const getCount = function () {
     const input = document.getElementById('count')
     return parseInt(input.value)
    }
+
+   document.getElementById('button').addEventListener('click', function() {
+    const count = getCount()
+    pushMessageToContent({type: 'like', count: count}) 
+   })
+
+
 
